9d807e2633a6de3368cebaa3462c56e2386747cb
John <johnz0923@gmail.com>
2019-10-08 20:05:44 +0100

    init

---
+++ constants.py
@@ -0,0 +1,9 @@
+NUMERIC_FEATURE_KEYS = ["Year of Record", "Age", "Size of City", "Wears Glasses", "Body Height [cm]"]
+
+CATEGORICAL_FEATURE_KEYS = ["Gender", "Country", "Profession", "University Degree", "Hair Color"]
+
+COLUMNS = ["Year of Record", "Gender", "Age", "Country", "Size of City", "Profession", "University Degree",
+           "Wears Glasses", "Hair Color", "Body Height [cm]", "Income"
+           ]
+FEATURE_COLUMNS = ["Year of Record", "Gender", "Age", "Country", "Size of City", "Profession", "University Degree",
+                   "Wears Glasses", "Hair Color", "Body Height [cm]"]
